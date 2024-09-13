11fd5ab7f99e76a9532fb54d0897c9fbd18d47dd
KeshanKulasekara <159102480+KeshanKulasekara@users.noreply.github.com>
2024-09-13 13:57:06 +0530

    Add new div tag and add new button to product box and edit the css

--- ractcart/src/Components/ProductsCart.js
+++ ractcart/src/Components/ProductsCart.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import './ProductCart.css';
 
-const ProductsCart= (props)=> {
-  const {name,image,price}= props.data;
+const ProductsCart = (props) => {
+  const { name, image, price } = props.data;
   return (
     <div className='itembox'>
-      <img src={image} alt='image1'  className='boximg' />
+      <img src={image} alt='image1' className='boximg' />
       <h3>{name}</h3>
-      $<p>{price}</p>
+      <div className='btnp'>
+        <p>${price}</p>
+        <button className='btncart'>Add To Cart</button>
+      </div>
     </div>
   )
 }

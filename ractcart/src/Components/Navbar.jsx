60132498528e0c152ec8aab77812acd108303a48
KeshanKulasekara <159102480+KeshanKulasekara@users.noreply.github.com>
2024-09-13 11:26:52 +0530

    add the class to ul tag and edit the css of the ul tag and add the border shadow under the border

--- ractcart/src/Components/Navbar.jsx
+++ ractcart/src/Components/Navbar.jsx
@@ -5,18 +5,18 @@ import shoppingimage from '../assets/Images/ShoppingCart.png'
 function Navbar() {
     return (
         <>
-        <nav className='navmain'>
-            <div className="leftnav">
-                <p className='textp'>Online Shopping Cart</p>
-            </div>
-            <div className="rightnav">
-                <ul>
-                    <li>Login</li>
-                    <li>Sign In</li>
-                </ul>
-                <img src={shoppingimage} alt="cart" className='imgnav' />
-            </div>
-        </nav>
+            <nav className='navmain'>
+                <div className="leftnav">
+                    <p className='textp'>Online Shopping Cart</p>
+                </div>
+                <div className="rightnav">
+                    <ul className='ulnav'>
+                        <li className='linav'>Sign in</li>
+                        <li className='linav'>Login</li>
+                        <img src={shoppingimage} alt="cart"  className='imgnav'/>
+                    </ul>
+                </div>
+            </nav>
         </>
     )
 }

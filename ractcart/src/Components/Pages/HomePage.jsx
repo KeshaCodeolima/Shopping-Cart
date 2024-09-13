2f531b4360b7ad26d2c53c6b05f2a329104de84a
KeshanKulasekara <159102480+KeshanKulasekara@users.noreply.github.com>
2024-09-13 11:42:40 +0530

    create the home page first tag and and import to app.js page

---
+++ ractcart/src/Components/Pages/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './HomePage.css';
+
+function HomePage() {
+    return (
+        <div>
+            <div>
+                <h1>Home Page</h1>
+            </div>
+        </div>
+    )
+}
+
+export default HomePage

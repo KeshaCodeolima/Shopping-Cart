7b0dc28caf0a4a7491818dacb9ce7bb9184b53aa
KeshanKulasekara <159102480+KeshanKulasekara@users.noreply.github.com>
2024-09-15 10:47:56 +0530

    Add some input fileds to the add item page

--- ractcart/src/Components/Pages/AddItems.jsx
+++ ractcart/src/Components/Pages/AddItems.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import './AddItems.css';
 
-function AddItems(){
-    return(
+function AddItems() {
+    return (
         <>
+            <div className="mainadditem">
+                <input type="text" placeholder='Enter Item Name' />
+                <input type="text" placeholder='Enter Item Price' />
+                <input type="text" placeholder='Enter Item Description' />
+                <input type="file" />
+                <button className='btnadd'>Add Item</button>
+            </div>
         </>
     )
 }

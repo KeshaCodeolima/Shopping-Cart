1b847a997f4feed8d6562d896a84b515cb4380f7
KeshanKulasekara <159102480+KeshanKulasekara@users.noreply.github.com>
2024-09-15 13:00:22 +0530

    Add new tags and edit the CSS page

--- ractcart/src/Components/Pages/AddItems.jsx
+++ ractcart/src/Components/Pages/AddItems.jsx
@@ -6,10 +6,13 @@ function AddItems() {
         <>
             <div className="mainadditem">
                 <div className="mainaddbox">
-                    <input type="text" placeholder='Enter Item Name' />
-                    <input type="text" placeholder='Enter Item Price' />
-                    <input type="text" placeholder='Enter Item Description' />
-                    <input type="file" />
+                    <div className="boxtopic">
+                        <h2>Add Items.</h2>
+                    </div>
+                    <input type="text" placeholder='Enter Item Name' className='inputadd' />
+                    <input type="text" placeholder='Enter Item Price' className='inputadd' />
+                    <textarea placeholder='Enter Item Description' className='inputaddtext' />
+                    <input type="file" className='inputadd1' />
                     <button className='btnadd'>Add Item</button>
                 </div>
             </div>

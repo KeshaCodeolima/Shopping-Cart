e04cf9000a79e4bc0f8425c4a0b166f1bd64ba21
KeshanKulasekara <159102480+KeshanKulasekara@users.noreply.github.com>
2024-09-19 10:59:02 +0530

    Add the input tags and classnames to edit and get the details

--- ractcart/src/Components/Pages/Register.jsx
+++ ractcart/src/Components/Pages/Register.jsx
@@ -2,11 +2,28 @@ import React from 'react';
 import './Register.css';
 
 function Register() {
-  return (
-    <>
-      
-    </>
-  )
+    return (
+        <>
+            <div className="registermain">
+                <div className="registerbox">
+                    <div className="registertittel">
+                        <h2>Register</h2>
+                    </div>
+                    <div className="registerdetails">
+                        <input type="text" placeholder='Enter Your Name' />
+                        <input type="email" placeholder='Enter Your Email' />
+                        <input type="text" placeholder='Enter Your Phone Number' />
+                        <input type="text" placeholder='Enter Your Address' />
+                        <input type="password" placeholder='Enter Your Password' />
+                        <input type="password" placeholder='Conform Password' />
+                    </div>
+                    <div className="btnregi">
+                        <button>Register</button>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default Register

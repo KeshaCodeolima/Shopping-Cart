73485d2e4f21c2cf480ec9e81e893e2c37c18bcd
KeshanKulasekara <159102480+KeshanKulasekara@users.noreply.github.com>
2024-09-19 10:50:51 +0530

    Add the register page to app.js page to navigation

--- ractcart/src/App.js
+++ ractcart/src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Navbar from "./Components/Navbar";
 import AddItems from "./Components/Pages/AddItems";
 import HomePage from "./Components/Pages/HomePage";
+import Register from "./Components/Pages/Register";
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage/>} />
         <Route path="/additem" element={<AddItems/>} />
+        <Route path="/register" element={<Register/>}/>
       </Routes>
     </div>
     

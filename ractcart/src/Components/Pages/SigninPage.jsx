49005a26444cb2460b19d77c695a94d11e4dac9a
KeshanKulasekara <159102480+KeshanKulasekara@users.noreply.github.com>
2024-09-19 11:50:05 +0530

    Add the signin page tags and classes

--- ractcart/src/Components/Pages/SigninPage.jsx
+++ ractcart/src/Components/Pages/SigninPage.jsx
@@ -4,7 +4,20 @@ import './SigninPage.css';
 function SigninPage(){
     return(
         <>
-
+        <div className="registermain">
+                <div className="registerbox">
+                    <div className="registertittel">
+                        <h2>Sign In</h2>
+                    </div>
+                    <div className="registerdetails">
+                        <input type="email" placeholder='Enter Your Email' />
+                        <input type="password" placeholder='Enter Your Password' />
+                    </div>
+                    <div className="btnregi">
+                        <button>Sign In</button>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }

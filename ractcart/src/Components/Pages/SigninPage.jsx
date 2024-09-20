d960785de0265bc79a60bb448ab0a0ef5a5c6c1f
KeshanKulasekara <159102480+KeshanKulasekara@users.noreply.github.com>
2024-09-20 10:15:24 +0530

    edit the API function to get the alert if user get correct sign in or not

--- ractcart/src/Components/Pages/SigninPage.jsx
+++ ractcart/src/Components/Pages/SigninPage.jsx
@@ -3,32 +3,36 @@ import './SigninPage.css';
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
-function SigninPage(){
+function SigninPage() {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
     const navigate = useNavigate()
 
     const Signin = (e) => {
         e.preventDefault();
-        axios.post("http://localhost:3001/signin", {email,password })
+        axios.post("http://localhost:3001/signin", { email, password })
             .then(result => {
                 console.log(result)
-                alert("Sign In Succsseful")
-                navigate('/')
+                if (result.data === "succsseful signin") {
+                    alert("Sign In Succsseful")
+                    navigate('/')
+                }else{
+                    alert("Check Your Password and Email")
+                }
             })
             .catch(err => console.log(err))
     }
 
-    return(
+    return (
         <>
-        <div className="registermain">
+            <div className="registermain">
                 <div className="signinbox">
                     <div className="registertittel">
                         <h2>Sign In</h2>
                     </div>
                     <div className="registerdetails">
-                        <input type="email" placeholder='Enter Your Email' onChange={(e) => setEmail(e.target.value)}/>
-                        <input type="password" placeholder='Enter Your Password' onChange={(e) => setPassword(e.target.value)}/>
+                        <input type="email" placeholder='Enter Your Email' onChange={(e) => setEmail(e.target.value)} />
+                        <input type="password" placeholder='Enter Your Password' onChange={(e) => setPassword(e.target.value)} />
                     </div>
                     <div className="btnregi">
                         <button onClick={Signin}>Sign in</button>

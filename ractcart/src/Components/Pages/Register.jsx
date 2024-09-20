a61875d0f7052c8446403f100e2bb24610ce92e3
KeshanKulasekara <159102480+KeshanKulasekara@users.noreply.github.com>
2024-09-20 09:37:28 +0530

    Add the API to do the registetion and import the navigate to do the navigation to signin page

--- ractcart/src/Components/Pages/Register.jsx
+++ ractcart/src/Components/Pages/Register.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import './Register.css';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 function Register() {
 
@@ -8,6 +10,19 @@ function Register() {
     const [address, setAddress] = useState('')
     const [phonenumber, setPhonenummber] = useState('')
     const [password, setPassword] = useState('')
+    const navigate = useNavigate()
+
+    const submit = (e) => {
+        e.preventDefault();
+        axios.post("http://localhost:3001/register", { name, email, address, phonenumber, password })
+            .then(result => {
+                console.log(result)
+                alert("Register Succsseful")
+                navigate('/signin')
+            })
+            .catch(err => console.log(err))
+    }
+
 
     return (
         <>
@@ -17,14 +32,14 @@ function Register() {
                         <h2>Register</h2>
                     </div>
                     <div className="registerdetails">
-                        <input type="text" placeholder='Enter Your Name' onChange={(e)=>setName(e.target.value)}/>
-                        <input type="email" placeholder='Enter Your Email' onChange={(e)=>setEmail(e.target.value)}/>
-                        <input type="text" placeholder='Enter Your Address' onChange={(e)=>setAddress(e.target.value)}/>
-                        <input type="text" placeholder='Enter Your Phone Number' onChange={(e)=>setPhonenummber(e.target.value)}/>
-                        <input type="password" placeholder='Enter Your Password' onChange={(e)=>setPassword(e.target.value)}/>
+                        <input type="text" placeholder='Enter Your Name' onChange={(e) => setName(e.target.value)} />
+                        <input type="email" placeholder='Enter Your Email' onChange={(e) => setEmail(e.target.value)} />
+                        <input type="text" placeholder='Enter Your Address' onChange={(e) => setAddress(e.target.value)} />
+                        <input type="text" placeholder='Enter Your Phone Number' onChange={(e) => setPhonenummber(e.target.value)} />
+                        <input type="password" placeholder='Enter Your Password' onChange={(e) => setPassword(e.target.value)} />
                     </div>
                     <div className="btnregi">
-                        <button>Register</button>
+                        <button type='submit' onClick={submit}>Register</button>
                     </div>
                 </div>
             </div>

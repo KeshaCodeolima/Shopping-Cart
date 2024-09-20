763dae3e110b99b8ef5ff0d2ba60a7664eb9a009
KeshanKulasekara <159102480+KeshanKulasekara@users.noreply.github.com>
2024-09-20 10:01:20 +0530

    Add the API link to do the signin and use the navigate to do the navitation to home page

--- ractcart/src/Components/Pages/SigninPage.jsx
+++ ractcart/src/Components/Pages/SigninPage.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import './SigninPage.css';
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 function SigninPage(){
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const navigate = useNavigate()
+
+    const Signin = (e) => {
+        e.preventDefault();
+        axios.post("http://localhost:3001/signin", {email,password })
+            .then(result => {
+                console.log(result)
+                alert("Sign In Succsseful")
+                navigate('/')
+            })
+            .catch(err => console.log(err))
+    }
+
     return(
         <>
         <div className="registermain">
@@ -10,11 +27,11 @@ function SigninPage(){
                         <h2>Sign In</h2>
                     </div>
                     <div className="registerdetails">
-                        <input type="email" placeholder='Enter Your Email' />
-                        <input type="password" placeholder='Enter Your Password' />
+                        <input type="email" placeholder='Enter Your Email' onChange={(e) => setEmail(e.target.value)}/>
+                        <input type="password" placeholder='Enter Your Password' onChange={(e) => setPassword(e.target.value)}/>
                     </div>
                     <div className="btnregi">
-                        <button>Sign in</button>
+                        <button onClick={Signin}>Sign in</button>
                     </div>
                 </div>
             </div>

54aa35e7bd4ca19392d2825c8858ef70ebdc01a3
KeshanKulasekara <159102480+KeshanKulasekara@users.noreply.github.com>
2024-09-21 10:52:01 +0530

    import cart slice to Store page

--- ractcart/src/Components/Store/Store.jsx
+++ ractcart/src/Components/Store/Store.jsx
@@ -1,7 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+import CartSlice from "./CartSlice";
 
 const Store = configureStore({
     reducer:{
-        
+        cart:CartSlice
     }
 })

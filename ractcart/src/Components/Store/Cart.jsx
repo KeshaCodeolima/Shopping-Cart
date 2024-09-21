41362e0c26d8d4c7c4aac41f91c090f1031c059c
KeshanKulasekara <159102480+KeshanKulasekara@users.noreply.github.com>
2024-09-21 11:42:25 +0530

    add new div tag and create new file to add some css to cart page

--- ractcart/src/Components/Store/Cart.jsx
+++ ractcart/src/Components/Store/Cart.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
+import React from 'react';
+import { useSelector } from 'react-redux';
+import './Cart.css';
 
 function Cart() {
     const cartitems = useSelector(state => state.cart.cart)
@@ -7,17 +8,18 @@ function Cart() {
         <>
             <h1>Cart page</h1>
             {
-                cartitems.map((item,index) =>(
+                cartitems.map((item, index) => (
                     <div key={index}>
-                         <img
-                            src={`http://localhost:3001/` + item.image}
-                            alt={item.name}
-                            className='boximg'
-                        />
-                        <h3> {item.name} </h3>
-                        <div className='btnp'>
-                            <p>{item.price}</p>
-                            <button className='btncart'>Remove</button>
+                        <div className="cartbox">
+                            <img
+                                src={`http://localhost:3001/` + item.image}
+                                alt={item.name}
+                                className='boximg' />
+                            <h3> {item.name} </h3>
+                            <div className='btnp'>
+                                <p>{item.price}</p>
+                                <button className='btncart'>Remove</button>
+                            </div>
                         </div>
                     </div>
                 ))

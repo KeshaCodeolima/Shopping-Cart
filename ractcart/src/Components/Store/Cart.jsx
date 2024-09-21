919c4cdeb437bf8a07f00cbf87567c201c000072
KeshanKulasekara <159102480+KeshanKulasekara@users.noreply.github.com>
2024-09-21 12:39:41 +0530

    Edit the cart page to display the itmes in the cart and add some css to cart page classes

--- ractcart/src/Components/Store/Cart.jsx
+++ ractcart/src/Components/Store/Cart.jsx
@@ -6,24 +6,38 @@ function Cart() {
     const cartitems = useSelector(state => state.cart.cart)
     return (
         <>
-            <h1>Cart page</h1>
-            {
-                cartitems.map((item, index) => (
-                    <div key={index}>
-                        <div className="cartbox">
-                            <img
-                                src={`http://localhost:3001/` + item.image}
-                                alt={item.name}
-                                className='boximg' />
-                            <h3> {item.name} </h3>
-                            <div className='btnp'>
-                                <p>{item.price}</p>
-                                <button className='btncart'>Remove</button>
+            <h1 className='carth1'>Cart page</h1>
+            <div className="cartmain">
+                <div className="cartleft">
+                    {
+                        cartitems.map((item, index) => (
+                            <div key={index}>
+                                <div className="cartbox">
+                                    <img
+                                        src={`http://localhost:3001/` + item.image}
+                                        alt={item.name}
+                                        className='cartboximg' />
+                                    <div className="carttexts">
+                                        <h3> {item.name} </h3>
+                                        <div className=' cartbtnp'>
+                                            <p>{item.price}</p>
+                                            <div className="addmore">
+                                                <span className='addspan'>-</span>
+                                                <span className='addspan'>1</span>
+                                                <span className='addspan'>+</span>
+                                            </div>
+                                            <button className='cartbtn'>Remove</button>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
-                        </div>
-                    </div>
-                ))
-            }
+                        ))
+                    }
+                </div>
+                <div className="cartright">
+
+                </div>
+            </div>
         </>
     )
 }

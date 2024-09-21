97d330a4fcae2b73821bfcf9832f2f5e20d15b05
KeshanKulasekara <159102480+KeshanKulasekara@users.noreply.github.com>
2024-09-21 10:54:02 +0530

    import and Add the react-redux Provider in to app.js

--- ractcart/src/App.js
+++ ractcart/src/App.js
@@ -4,20 +4,23 @@ import AddItems from "./Components/Pages/AddItems";
 import HomePage from "./Components/Pages/HomePage";
 import Register from "./Components/Pages/Register";
 import SigninPage from "./Components/Pages/SigninPage";
+import { Provider } from "react-redux";
 
 
 function App() {
   return (
     <div>
-      <Navbar/>
-      <Routes>
-        <Route path="/" element={<HomePage/>} />
-        <Route path="/additem" element={<AddItems/>} />
-        <Route path="/register" element={<Register/>}/>
-        <Route path="/signin" element={<SigninPage/>}/>
-      </Routes>
+      <Provider>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/additem" element={<AddItems />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/signin" element={<SigninPage />} />
+        </Routes>
+      </Provider>
     </div>
-    
+
   );
 }
 

5b02fc5a00f247de460519a3f72e8bd0e24e18b7
KeshanKulasekara <159102480+KeshanKulasekara@users.noreply.github.com>
2024-09-21 11:02:52 +0530

    import the addcart method to home page

--- ractcart/src/Components/Pages/HomePage.jsx
+++ ractcart/src/Components/Pages/HomePage.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import './HomePage.css';
 import axios from 'axios';
 import { useDispatch } from 'react-redux';
+import { addcart } from '../Store/CartSlice';
 
 function HomePage() {
 

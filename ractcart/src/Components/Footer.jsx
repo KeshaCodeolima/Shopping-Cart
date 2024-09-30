b36b2e2a77115a7d525061962db1a159a8cb9cd6
KeshanKulasekara <159102480+KeshanKulasekara@users.noreply.github.com>
2024-09-30 09:56:12 +0530

    create the main div tags

--- ractcart/src/Components/Footer.jsx
+++ ractcart/src/Components/Footer.jsx
@@ -5,7 +5,11 @@ function Footer() {
   return (
     <>
       <footer>
-
+        <div className="footermain">
+            <div className="footerleft"></div>
+            <div className="footercenter"></div>
+            <div className="footerright"></div>
+        </div>
       </footer>
     </>
   )

2f6fe61141a74af68c127404d887d3a99c19086a
KeshanKulasekara <159102480+KeshanKulasekara@users.noreply.github.com>
2024-09-30 10:03:06 +0530

    create the left side texts

--- ractcart/src/Components/Footer.jsx
+++ ractcart/src/Components/Footer.jsx
@@ -6,7 +6,19 @@ function Footer() {
     <>
       <footer>
         <div className="footermain">
-            <div className="footerleft"></div>
+
+            <div className="footerleft">
+                <div className="aboutleft">
+                    <p className='textp'>About Us</p>
+                    <ul className='textul'>
+                        <li>Our Mission</li>
+                        <li>Press</li>
+                        <li>Careers</li>
+                        <li>Our Story</li>
+                    </ul>
+                </div>
+            </div>
+
             <div className="footercenter"></div>
             <div className="footerright"></div>
         </div>
